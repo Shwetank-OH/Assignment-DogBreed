978b637607451a147526e3bed403d12878403be8
Shwetank <shwetank.mishra@oncehub.com>
2023-03-03 17:35:40 +0530

    service and module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BreedComponent } from './breed/breed.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import {HttpClientModule} from '@angular/common/http';
+import { ApiService } from './app.service';
+import { BreedPipe } from './breed/breed.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BreedComponent,
+    ContactComponent,
+    AboutComponent,
+    BreedPipe
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
